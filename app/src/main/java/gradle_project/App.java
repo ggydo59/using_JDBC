2c73a48bd4155ef9adb01c0f2d184109e01e3599
Mingyeongdo <mgd813@gmail.com>
2023-02-24 02:56:03 +0900

    feat: 필요없는 코드 삭제, 코드 주석삽입

--- app/src/main/java/gradle_project/App.java
+++ app/src/main/java/gradle_project/App.java
@@ -16,15 +16,20 @@ public class App {
         System.out.println(new App().getGreeting());
         try {
             //here de-jdbc is database name, root is username and password is null. Fix them for your database settings.
+            // 1. conn 생성
             Connection con = DriverManager.getConnection(
             "jdbc:mysql://localhost:3306/de_jdbc", "root","0000");
+            // 2. statement 생성
             Statement stmt = con.createStatement();
+            // 3. statement type 변수 stmt에 쿼리문을 대입
             ResultSet rs = stmt.executeQuery("select * from product");
+            // 4. 반복문을 돌며 1번 컬럼부터 5번 컬럼까지의 모든 내용을 출력한다.
             while (rs.next()){
             System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
             + rs.getDate(3) + " " + rs.getString(4)
             + " " + rs.getInt(5));
             }
+            // 5. 연결을 종료한다.
             con.close();
         }   catch (Exception e) {System.out.println(e);}
     }
